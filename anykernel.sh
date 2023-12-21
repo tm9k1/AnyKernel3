e724987c1a7553a952d26452392155b629317e22
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2023-12-21 00:36:12 -0300

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -29,7 +29,7 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 } # end attributes
 
 # boot shell variables
-block=/dev/block/platform/soc/1d84000.ufshc/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
